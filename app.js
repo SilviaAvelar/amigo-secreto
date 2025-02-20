e0542e9e20245c4ae0ea85458f6258bf905628b1
Silvia-Avelar <avelar.silvia@yahoo.com>
2025-02-20 20:17:10 +0000

    Primeiro commit - subindo repositório

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const nomeAmigo = document.getElementById('amigo').value.trim();
+
+    if (nomeAmigo === "") {
+        alert("Por favor digite um nome.");
+        return;
+    }
+
+    if (amigos.includes(nomeAmigo)) {
+        alert("Este nome já foi adicionado.");
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    atualizarListaAmigos();
+    document.getElementById('amigo').value = '';
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("É necessário pelo menos 2 amigos para fazer o sorteio.");
+        return;
+    }
+    const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    exibirResultado(`O amigo secreto sorteado é: ${amigoSorteado}`);
+}
+
+function exibirResultado(resultado) {
+    const resultadoElemento = document.getElementById('resultado');
+    resultadoElemento.innerHTML = ''; 
+    const li = document.createElement('li');
+    li.textContent = resultado; 
+    resultadoElemento.appendChild(li);
+}
